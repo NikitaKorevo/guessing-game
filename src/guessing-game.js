d2f7beeeab20f653e8ee91ee9decad9cf236697e
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-07-05 21:42:33 +0300

    feat: find number through binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.indexNumber;
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i < max; i++) {
+            this.arr.push(i);
+        }
     }
 
     guess() {
-
+        this.indexNumber = Math.round((this.arr.length) / 2);
+        return this.arr[this.indexNumber];
     }
 
     lower() {
-
+        this.arr.length = this.indexNumber;
     }
 
     greater() {
-
+       this.arr.splice(0, this.indexNumber);
     }
 }
 
